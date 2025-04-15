92ea6e3b1636189268ae4d0cbc019253c8b5195a
francyes <ciaofra1237456@gmail.com>
2025-04-15 18:32:10 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
+      }
+    }
+  }
+});
